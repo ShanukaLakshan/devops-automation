1c0853628d3e604453304c2e575650dad8b081eb
ShanukaLakshan <lakshan.20@uom.lk>
2024-12-29 03:00:11 +0530

    Update welcome message in DevopsIntegrationApplication

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to Test Devops Integration";
+		return "Welcome to Test Devops Integration again";
 	}
 
 	public static void main(String[] args) {
